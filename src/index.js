94c90c96565b2569e7e8d04889dc440eef577273
nika <nika.gogua8@gmail.com>
2023-02-09 15:45:25 +0400

    change router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { CVContextProvider } from "./components/Store/cvContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <CVContextProvider>
       <App />
     </CVContextProvider>
-  </HashRouter>
+  </BrowserRouter>
 );

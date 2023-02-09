5dd896277d8c07679dc1e52e9dfa18a316120f74
nika <nika.gogua8@gmail.com>
2023-02-09 15:41:02 +0400

    github page

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { CVContextProvider } from "./components/Store/cvContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <CVContextProvider>
       <App />
     </CVContextProvider>

9bfb8cafac6a8e934a1aaee8ca7b7ea6a652a274
nika <nika.gogua8@gmail.com>
2023-02-10 22:05:32 +0400

    border for cv page

--- src/components/UI/CVcard/CVContainer.jsx
+++ src/components/UI/CVcard/CVContainer.jsx
@@ -1,13 +1,19 @@
 import { useContext } from "react";
+import { useLocation } from "react-router-dom";
 import cvIcon from "../../../assets/cv-icon.png";
 import cvContext from "../../Store/cvContext";
 import classes from "./CVContainer.module.css";
 
 const CVContainer = (props) => {
   const { isSubmit } = useContext(cvContext);
+  const location = useLocation();
 
   return (
-    <div className={`${classes.cv} ${isSubmit && classes.cvBorder}`}>
+    <div
+      className={`${classes.cv} ${
+        isSubmit && location.pathname === "/cv" && classes.cvBorder
+      }`}
+    >
       <div className={classes.cvDiv}>{props.children}</div>
       <img src={cvIcon} alt="" />
     </div>

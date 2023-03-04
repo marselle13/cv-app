424300f1a9361ef499b0094a5fcffccc406c2a60
nika <nika.gogua8@gmail.com>
2023-03-04 16:10:34 +0400

    loading spinner for landing page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import { Route, Routes } from "react-router-dom";
-import LandingPage from "./components/Landing/LandingPage";
 import Personal from "./components/Personal/Personal";
 import Experience from "./components/Experience/Experience";
 import Education from "./components/Education/Education";
 import CVResult from "./components/CV/CVResult";
+import React, { Suspense } from "react";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const LandingPage = React.lazy(() =>
+  import("./components/Landing/LandingPage")
+);
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route path="/personal" element={<Personal />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/cv" element={<CVResult />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route path="/personal" element={<Personal />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/cv" element={<CVResult />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
